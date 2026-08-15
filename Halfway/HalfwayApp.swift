8115982c0da008fdaa9421bcb4b8c39f5291580b
Jaewon Park <jaewonparkk73@gmail.com>
2026-08-15 01:38:49 -0400

    Initial Commit

---
+++ Halfway/HalfwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfwayApp.swift
+//  Halfway
+//
+//  Created by Jaewon Park on 8/15/26.
+//
+
+import SwiftUI
+
+@main
+struct HalfwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
